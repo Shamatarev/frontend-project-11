a843fd53261e93fe08857824237fc70b95961251
Shamatarev <sh.a.v@list.ru>
2023-05-28 15:21:46 +0300

    1

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  // output: './dist/',
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
