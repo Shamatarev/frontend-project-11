78e60c79f6c542fb1d3c9be00dbc13db1291c225
Shamatarev <sh.a.v@list.ru>
2023-05-29 12:47:53 +0300

    validation

--- src/index.js
+++ src/index.js
@@ -1,48 +1,7 @@
 import './styles.scss';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'bootstrap';
-import * as yup from 'yup';
-import onChange from 'on-change';
+import app from './view.js';
 
-const rssSchema = yup.object().shape({
-  rssLink: yup.string().required('Укажите RSS-ссылку')
-    .url('Некорректный формат ссылки')
-    .matches(
-      /^(https?:\/\/)?([A-Za-z0-9_-]+\.)+[A-Za-z]{2,}(\/.*)*\/?$/,
-      'Неверный формат RSS-ссылки',
-    ),
-    // .test('duplicate', 'RSS ссылка уже добавлена', async (value) => {
-    //   //  проверка на дубликаты ссылок
-    //   //  проверить значение value в списке ранее введенных ссылок
-    //   const isDuplicate = await checkForDuplicate(value);
-    //   return !isDuplicate;
-    // }),
-});
-
-async function app() {
-  const form = document.querySelector('.rss-form');
-  // console.log(form);
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const urlInput = document.querySelector('#url-input');
-    const rssLink = urlInput.value.trim();
-    console.log('urlValue', rssLink);
-    console.log('urlValue', { rssLink });
-    rssSchema
-      .validate({ rssLink })
-      .then((validData) => {
-        // Валидация прошла успешно
-        console.log('Форма отправлена:', validData);
-        urlInput.classList.remove('is-invalid');
-        // Здесь можно выполнить дополнительные действия, например, отправить данные на сервер
-      })
-      .catch((validationError) => {
-        // Валидация завершилась с ошибками
-        console.error('Ошибка валидации:', validationError.errors);
-        urlInput.classList.add('is-invalid');
-        // Здесь можно обработать ошибки валидации и вывести их на страницу
-      });
-  });
-}
-
-export default app();
+app();
+console.log('Hello, World!');
